c143ced54c6511faead43ac44e9e844a5e556a97
Jeremy <Jeremy.hernandez504@gmail.com>
2024-08-07 13:45:48 -0500

    added working button

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ $(document).ready(() => {
   //structure variables
   const $body = $('body');
   $body.html('');
+  const $tweetSec = $(`<section></section>`);
+  const $tweetBank = $(`<div id='new-tweets'</div>`);
   //button variables
-  const buttonSec = $(`<section `);
+  const $buttonSec = $(`<section></section>`);
+  const $buttonDiv = $(`<div id='tweet-button></div>`);
+  const $button = $(`<button id='update-tweet'>Update Recent Tweets</button>`);
+  $($buttonSec).prepend($button); //attach button to button section
+  $('body').append($buttonSec); //attaches button section to top of body
+
+  //function for event listener to update feed
+  $button.click(function(){
+    let newTweet = streams.home;
+    nameTracker = null;
+    $tweetBank.html('');
+    $tweetBank.prepend(makeTweet(newTweet));
+  })
+
+
     function makeTweet(){
     const $tweets = streams.home.map((tweet) => {
       const $tweet = $('<div></div>');
@@ -15,9 +31,12 @@ $(document).ready(() => {
 
       return $tweet;
     });
-    $body.append($tweets);
+    $('body').append($tweetSec);
+    $($tweetSec).append($tweetBank);
+    $($tweetBank).append($tweets);
   }
   makeTweet();
+
 
   
 });
