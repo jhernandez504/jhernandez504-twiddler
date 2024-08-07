448ec7f8e015ea7374d59c6899112e3d9a6c288f
Jeremy <Jeremy.hernandez504@gmail.com>
2024-08-07 13:23:19 -0500

    made makeTweet function

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 
 $(document).ready(() => {
+  //variables
+  //structure variables
   const $body = $('body');
   $body.html('');
+  //button variables
+  const buttonSec = $(`<section `);
+    function makeTweet(){
+    const $tweets = streams.home.map((tweet) => {
+      const $tweet = $('<div></div>');
+      const text = `@${tweet.user}: ${tweet.message}`;
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+      $tweet.text(text);
 
-    $tweet.text(text);
+      return $tweet;
+    });
+    $body.append($tweets);
+  }
+  makeTweet();
 
-    return $tweet;
-  });
-  $body.append($tweets);
-
+  
 });
